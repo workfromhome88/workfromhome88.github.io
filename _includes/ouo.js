31cf2b0e22d40c79854f78e1d46885b60452c228
hiplii-dsw <50976907+hiplii-dsw@users.noreply.github.com>
2019-10-27 12:02:26 +0700

    a

--- _includes/ouo.js
+++ _includes/ouo.js
@@ -1,5 +1,5 @@
 <script type="text/javascript">
     var ouo_token = 'nvuoC7RL';
-    var domains = ['depositfiles.com', 'rapidshare.com', 'uploading.com', 'uploadable.ch', 'letitbit.net', 'turbobit.net']; 
+    var exclude_domains = ['hibga.com', 'workfromhome88.com']; 
 </script>
 <script src="//cdn.ouo.io/js/full-page-script.js"></script>
