6f558e187562389eab3969d90e52729a352316de
hiplii-dsw <50976907+hiplii-dsw@users.noreply.github.com>
2019-11-01 00:04:25 +0700

    a

--- _includes/ouo.js
+++ _includes/ouo.js
@@ -1,5 +1,5 @@
 <script type="text/javascript">
     var ouo_token = 'nvuoC7RL';
-    var exclude_domains = ['hibga.com', 'workfromhome88.com']; 
+    var exclude_domains = ['workfromhome88.com']; 
 </script>
 <script src="//cdn.ouo.io/js/full-page-script.js"></script>
